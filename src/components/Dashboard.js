4e76a7d4952c3e7963e2f0cbb92831ffccb8a16a
getenesh <getenesh.guta@gmail.com>
2023-06-30 10:54:45 -0400

    bindding this

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -32,9 +32,12 @@ class Dashboard extends Component {
     this.selectPanel = this.selectPanel.bind(this);
   }
 
-  selectPanel(id) {
-    this.setState({ focused: id });
-  }
+  selectPanel = (id) => {
+    this.setState((previousState) => ({
+      focused: previousState.focused !== null ? null : id,
+    }));
+  };
+  
 
   render() {
     const { focused } = this.state;
@@ -42,12 +45,11 @@ class Dashboard extends Component {
       .filter((panel) => (focused ? panel.id === focused : true))
       .map((panel) => (
         <Panel
-          key={panel.id}
-          id={panel.id}
-          label={panel.label}
-          value={panel.value}
-          onSelect={this.selectPanel}
-        />
+        key={panel.id}
+        label={panel.label}
+        value={panel.value}
+        onSelect={event => this.selectPanel(panel.id)}
+      />
       ));
 
     return <main className="dashboard">{panels}</main>;
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,20 +1,15 @@
 import React, { Component } from "react";
-import classNames from "classnames";
 
 class Panel extends Component {
-  handleClick = () => {
-    const { id, onSelect } = this.props;
-    onSelect(id); // Call the onSelect function with the id of the panel
-  };
-
   render() {
-    const { id, label, value, onSelect } = this.props;
-
-    return (
-      <section className="dashboard__panel" onClick={() => onSelect(id)}>
-        <h1 className="dashboard__panel-header">{label}</h1>
-        <p className="dashboard__panel-value">{value}</p>
-      </section>)
+   const { label, value, onSelect } = this.props;
+ 
+   return (
+    <section className="dashboard__panel" onClick={onSelect}>
+     <h1 className="dashboard__panel-header">{label}</h1>
+     <p className="dashboard__panel-value">{value}</p>
+    </section>
+   );
   }
-}
+ }
 export default Panel;
